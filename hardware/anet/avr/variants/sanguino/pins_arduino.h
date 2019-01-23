4561fdbe81f8127ecb3c309f28e07a206abdf7da
uwedamm <46942357+uwedamm@users.noreply.github.com>
2019-01-23 21:04:20 +0100

    Update pins_arduino.h
    Hello,
    I am using Arduino 1.8.5
    compiling the MARLIN for anet A8, I get compiler warnings like:
    > In file included from /home/uwe/.arduino15/packages/arduino/hardware/avr/1.6.23/cores/arduino/Arduino.h:257:0,
    >                  from /home/uwe/.arduino15/packages/arduino/hardware/avr/1.6.23/cores/arduino/main.cpp:20:
    > /home/uwe/Arduino/hardware/anet/avr/variants/sanguino/pins_arduino.h:57:0: warning: "TIMER5C" redefined
    >  #define TIMER5C 16
    >  ^
    > In file included from /home/uwe/.arduino15/packages/arduino/hardware/avr/1.6.23/cores/arduino/main.cpp:20:0:
    > /home/uwe/.arduino15/packages/arduino/hardware/avr/1.6.23/cores/arduino/Arduino.h:223:0: note: this is the location of the previous definition
    >  #define TIMER5C 18
    (modified pings_arduino.h already, the line-numbers are probably wrong)
    I am in doubt, if I can ignore that warning, due to the fact, that the #defines in Arduino.h and pins_arduino.h are different.
    These defines are used in digital_pin_to_timer_PGM[] array.
    Which is abstracted by
    Arduino.h:#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
    
    This is used for example in
    packages/arduino/hardware/avr/1.6.23/cores/arduino/wiring_analog.c
    where the constants are used in switch case.
    -> #define values are not that critical, just that e.g. TIMER1C is missing in pins_arduino.h
    
    The values in pins_arduino.h are maybe critical, if they are mixed up with the ones in Arduino.h
    
    Therefor I would propose to remove the #defines in pins_arduino.h:
    
    in addition I put static in front of const, to avoid another compiler warning too.
    
    thank you for any feedback
    Uwe

--- hardware/anet/avr/variants/sanguino/pins_arduino.h
+++ hardware/anet/avr/variants/sanguino/pins_arduino.h
@@ -13,7 +13,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
+566555
   You should have received a copy of the GNU Lesser General
   Public License along with this library; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -36,28 +36,35 @@
 #define NOT_A_PORT 0
 
 #define NOT_ON_TIMER 0
-#define TIMER0A 1
-#define TIMER0B 2
-#define TIMER1A 3
-#define TIMER1B 4
-#define TIMER2  5
-#define TIMER2A 6
-#define TIMER2B 7
 
-#define TIMER3A 8
-#define TIMER3B 9
-#define TIMER3C 10
-#define TIMER4A 11
-#define TIMER4B 12
-#define TIMER4C 13
-#define TIMER5A 14
-#define TIMER5B 15
-#define TIMER5C 16
+/* proposal from: https://www.thingiverse.com/groups/anet-a8-prusa-i3/forums/general/topic:26351 */
+/*
+  #define TIMER0A 1
+  #define TIMER0B 2
+  #define TIMER1A 3
+  #define TIMER1B 4
+  #define TIMER2  5
+  #define TIMER2A 6
+  #define TIMER2B 7
 
-const static uint8_t SS   = 4;
-const static uint8_t MOSI = 5;
-const static uint8_t MISO = 6;
-const static uint8_t SCK  = 7;
+  #define TIMER3A 8
+  #define TIMER3B 9
+  #define TIMER3C 10
+  #define TIMER4A 11
+  #define TIMER4B 12
+  #define TIMER4C 13
+  #define TIMER5A 14
+  #define TIMER5B 15
+  #define TIMER5C 16
+*/
+
+/* static before const to avoid: */
+/* .../Arduino/hardware/anet/avr/variants/sanguino/pins_arduino.h:61:1: warning: 'static' is not at beginning of declaration [-Wold-style-declaration] */
+static const uint8_t SS   = 4;
+static const uint8_t SS   = 4;
+static const uint8_t MOSI = 5;
+static const uint8_t MISO = 6;
+static const uint8_t SCK  = 7;
 
 static const uint8_t SDA = 17;
 static const uint8_t SCL = 16;
